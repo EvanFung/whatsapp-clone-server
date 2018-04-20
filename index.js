7438fb944e4aa6b9dc7ddcf7b099a8b4180e6239
evan fung <evanfungv@gmail.com>
2018-04-20 17:40:52 +0800

    chore: server config scaffolding.

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
+import http from "http";
+import path from "path";
+import methods from "methods";
 import express from "express";
-
+import bodyParser from "body-parser";
+import session from "express-session";
+import cors from "cors";
+import passport from "passport";
+import errorhandler from "errorhandler";
+import mongoose from "mongoose";
 const PORT = 8080;
-
+//Create global app object
 const app = express();
+
+app.use(cors());
+
+//normal express config defaults
+app.use(require("morgan")("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(require("method-override")());
+app.use(express.static(__dirname + "/public"));
+
+app.use(
+  session({
+    secret: "conduit",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use(errorhandler());
+
+//for convinience we using the mlab uri
+mongoose.connect(
+  "mongodb://evanfung:password@ds151809.mlab.com:51809/whatsapp-clone"
+);
+mongoose.set("debug", true);
 
 app.listen(PORT, () =>
   console.log(`Server is now running on http://localhost:${PORT}`)
