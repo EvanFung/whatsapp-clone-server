a559e16257a7bc619d224ab78479c26c235c348c
evan fung <evanfungv@gmail.com>
2018-04-22 23:58:42 +0800

    feat: add local passport   to authenticate user

---
+++ config/passport.js
@@ -0,0 +1,27 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import mongoose from "mongoose";
+
+const User = mongoose.model("User");
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "user[email]",
+      passwordField: "user[password]"
+    },
+    function(email, password, done) {
+      User.findOne({ email })
+        .then(function(user) {
+          if (!user || !user.validPassword(password)) {
+            return done(null, false, {
+              errors: { "email or password": "is invalid" }
+            });
+          }
+
+          return done(null, user);
+        })
+        .catch(done);
+    }
+  )
+);
